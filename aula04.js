ec01d594ebd66c0ac8951812e348e3f8911b4215
luamondstock <134516705+luamondstock@users.noreply.github.com>
2024-08-22 12:04:29 -0300

    Add files via upload

--- aula04.js
+++ aula04.js
@@ -35,8 +35,7 @@ function somaDeNumeros(numero01, numero02){
 function nomeIdade(nome,idade){
     return `Meu nome é ${nome}, e minha idade é ${idade}`
 }
- imprimeTexto(nomeIdade("Lua", 17))
-  // A ordem dos parâmetros é importante!!
+ // A ordem dos parâmetros é importante!!
 imprimeTexto(nomeIdade("Lua", 17))
 imprimeTexto(nomeIdade(22, "ricardão"))
 
@@ -47,5 +46,27 @@ function multiplicao(numero4 = 2, numero5 = 5){
 }
 imprimeTexto(multiplicao(somaDeNumeros(3,4),somaDeNumeros(5,10)));
 
+// expressão de função
+
+const constanteSoma = function(nume1, nume2){return nume1 + nume2}
+
+console.log(constanteSoma (3,6))
+
+// Funções e var são "listadas" no topo do arquivo
+
+// Arrow function
+
+const apresentaArrow = nome => `meu nome é ${jota}`;
+const somaEmFlecha = (nu1, nu2) => nu1 + nu2;
+
+console.log(apresentaArrow("Lua"))
+console.log(somaEmFlecha("2,4"))
+
+
+const somaNumerosPequenos = (num1, num2) => {
+    if (num1 || num2 > 10){
+        return "essas"
+    }
+}
 
 
